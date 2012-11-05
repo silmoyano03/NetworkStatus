917e8dc7b6c4049c803718a2046e222bad5cbbc5
Franco Rodriguez <francoel69@gmail.com>
2012-11-04 22:19:16 -0300

    Domingo 4 de noviembre Alta Gracia

--- GSMStatus/gen/com/trabajofinal/gsm/gsmstatus/R.java
+++ GSMStatus/gen/com/trabajofinal/gsm/gsmstatus/R.java
@@ -20,12 +20,19 @@ public final class R {
         public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int cid=0x7f080003;
-        public static final int gsmcelllocation=0x7f080000;
-        public static final int mcc=0x7f080001;
-        public static final int menu_settings=0x7f080005;
-        public static final int mnc=0x7f080002;
-        public static final int neighboring=0x7f080004;
+        public static final int LinearLayout=0x7f080001;
+        public static final int ScrollView=0x7f080000;
+        public static final int cid=0x7f080008;
+        public static final int gsmStatus=0x7f080002;
+        public static final int gsmcelllocation=0x7f080003;
+        public static final int iterleft=0x7f080007;
+        public static final int mcc=0x7f080005;
+        public static final int menu_settings=0x7f08000c;
+        public static final int mnc=0x7f080006;
+        public static final int neighboring=0x7f080009;
+        public static final int operator=0x7f080004;
+        public static final int wifiInfo=0x7f08000b;
+        public static final int wifiStatus=0x7f08000a;
     }
     public static final class layout {
         public static final int layout_gsmstatus=0x7f030000;
@@ -35,9 +42,11 @@ public final class R {
     }
     public static final class string {
         public static final int app_name=0x7f050000;
+        public static final int gsmStatus=0x7f050004;
         public static final int hello_world=0x7f050001;
         public static final int menu_settings=0x7f050002;
         public static final int title_activity_gsmstatus=0x7f050003;
+        public static final int wifiStatus=0x7f050005;
     }
     public static final class style {
         public static final int AppTheme=0x7f060000;
--- GSMStatus/src/com/trabajofinal/gsm/gsmstatus/GSMStatus.java
+++ GSMStatus/src/com/trabajofinal/gsm/gsmstatus/GSMStatus.java
@@ -3,9 +3,14 @@ package com.trabajofinal.gsm.gsmstatus;
 import java.util.List;
 
 import android.app.Activity;
+import android.content.BroadcastReceiver;
 import android.content.Context;
-import android.net.wifi.WifiConfiguration;
-import android.net.wifi.WifiInfo;
+import android.content.IntentFilter;
+import android.content.Intent;
+import android.os.CountDownTimer;
+import android.net.wifi.ScanResult;
+//import android.net.wifi.WifiConfiguration;
+//import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.telephony.NeighboringCellInfo;
@@ -24,68 +29,139 @@ public class GSMStatus extends Activity {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.layout_gsmstatus);
-		TextView textGsmCellLocation = (TextView) findViewById(R.id.gsmcelllocation);
-		TextView textMCC = (TextView) findViewById(R.id.mcc);
-		TextView textMNC = (TextView) findViewById(R.id.mnc);
-		TextView textCID = (TextView) findViewById(R.id.cid);
-		TextView textWiFi = (TextView) findViewById(R.id.wifiInfo);
-
+		final TextView textGsmCellLocation = (TextView) findViewById(R.id.gsmcelllocation);
+		final TextView textMCC = (TextView) findViewById(R.id.mcc);
+		final TextView textMNC = (TextView) findViewById(R.id.mnc);
+		final TextView textCID = (TextView) findViewById(R.id.cid);
+		final TextView textWiFi = (TextView) findViewById(R.id.wifiInfo);
+		final TextView textOperator = (TextView) findViewById(R.id.operator);
+		final TextView iterLeft = (TextView) findViewById(R.id.iterleft);
+		
 		// Get GSM status
 
 		// retrieve a reference to an instance of TelephonyManager
-		TelephonyManager telephonyManager = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
-		telephonyManager.listen(mPhoneStateReceiver,
-				PhoneStateListener.LISTEN_SIGNAL_STRENGTHS);
-		GsmCellLocation cellLocation = (GsmCellLocation) telephonyManager
-				.getCellLocation();
-
+		final TelephonyManager telephonyManager = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
+		// telephonyManager.listen(mPhoneStateReceiver,
+		// PhoneStateListener.LISTEN_SIGNAL_STRENGTHS);
 		String networkOperator = telephonyManager.getNetworkOperator();
+		String operator = telephonyManager.getNetworkOperatorName();
 		String mcc = networkOperator.substring(0, 3);
 		String mnc = networkOperator.substring(3);
 		textMCC.setText("mcc: " + mcc);
 		textMNC.setText("mnc: " + mnc);
+		textOperator.setText("Operador: " + operator);
+		CountDownTimer timer = new CountDownTimer(300000, 3000) {
 
-		int cid = cellLocation.getCid();
-		int lac = cellLocation.getLac();
-		textGsmCellLocation.setText(cellLocation.toString());
-		textCID.setText("gsm cell id: " + String.valueOf(cid));
+			@Override
+			public void onTick(long millisUntilFinished) {
+				TelephonyManager telephonyManager2 = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
 
-		TextView Neighboring = (TextView) findViewById(R.id.neighboring);
-		List<NeighboringCellInfo> NeighboringList = telephonyManager
-				.getNeighboringCellInfo();
+				iterLeft.setText("seconds remaining: " +millisUntilFinished/3000);
+				GsmCellLocation cellLocation = (GsmCellLocation) telephonyManager2
+						.getCellLocation();
 
-		String stringNeighboring = "Neighboring List- Lac : Cid : RSSI\n";
-		for (int i = 0; i < NeighboringList.size(); i++) {
+				int cid = cellLocation.getCid();
+				// int lac = cellLocation.getLac();
+				textGsmCellLocation.setText(cellLocation.toString());
+				textCID.setText("gsm cell id: " + String.valueOf(cid));
 
-			String dBm;
-			int rssi = NeighboringList.get(i).getRssi();
-			if (rssi == NeighboringCellInfo.UNKNOWN_RSSI) {
-				dBm = "Unknown RSSI";
-			} else {
-				dBm = String.valueOf(-113 + 2 * rssi) + " dBm";
+				TextView Neighboring = (TextView) findViewById(R.id.neighboring);
+				List<NeighboringCellInfo> NeighboringList = telephonyManager2
+						.getNeighboringCellInfo();
+				
+				String stringNeighboring = "Neighboring List- Lac : Cid : RSSI\n";
+				for (int i = 0; i < NeighboringList.size(); i++) {
+
+					String dBm;
+					int rssi = NeighboringList.get(i).getRssi();
+					if (rssi == NeighboringCellInfo.UNKNOWN_RSSI) {
+						dBm = "Unknown RSSI";
+					} else {
+						dBm = String.valueOf(-113 + 2 * rssi) + " dBm";
+					}
+
+					stringNeighboring = stringNeighboring
+							+ String.valueOf(NeighboringList.get(i).getLac())
+							+ " : "
+							+ String.valueOf(NeighboringList.get(i).getCid())
+							+ " : " + dBm + "\n";
+				}
+
+				Neighboring.setText(stringNeighboring);
 			}
 
-			stringNeighboring = stringNeighboring
-					+ String.valueOf(NeighboringList.get(i).getLac()) + " : "
-					+ String.valueOf(NeighboringList.get(i).getCid()) + " : "
-					+ dBm + "\n";
-		}
+			@Override
+			public void onFinish() {
+			}
+		}.start();
 
-		Neighboring.setText(stringNeighboring);
+//		GsmCellLocation cellLocation = (GsmCellLocation) telephonyManager
+//				.getCellLocation();
+//
+//		String networkOperator = telephonyManager.getNetworkOperator();
+//		String operator = telephonyManager.getNetworkOperatorName();
+//		String mcc = networkOperator.substring(0, 3);
+//		String mnc = networkOperator.substring(3);
+//		textMCC.setText("mcc: " + mcc);
+//		textMNC.setText("mnc: " + mnc);
+//		textOperator.setText("Operador: " + operator);
+//
+//		int cid = cellLocation.getCid();
+//		// int lac = cellLocation.getLac();
+//		textGsmCellLocation.setText(cellLocation.toString());
+//		textCID.setText("gsm cell id: " + String.valueOf(cid));
+//
+//		TextView Neighboring = (TextView) findViewById(R.id.neighboring);
+//		List<NeighboringCellInfo> NeighboringList = telephonyManager
+//				.getNeighboringCellInfo();
+//
+//		String stringNeighboring = "Neighboring List- Lac : Cid : RSSI\n";
+//		for (int i = 0; i < NeighboringList.size(); i++) {
+//
+//			String dBm;
+//			int rssi = NeighboringList.get(i).getRssi();
+//			if (rssi == NeighboringCellInfo.UNKNOWN_RSSI) {
+//				dBm = "Unknown RSSI";
+//			} else {
+//				dBm = String.valueOf(-113 + 2 * rssi) + " dBm";
+//			}
+//
+//			stringNeighboring = stringNeighboring
+//					+ String.valueOf(NeighboringList.get(i).getLac()) + " : "
+//					+ String.valueOf(NeighboringList.get(i).getCid()) + " : "
+//					+ dBm + "\n";
+//		}
+//
+//		Neighboring.setText(stringNeighboring);
 
 		// Get Wi-Fi status
 
-		WifiManager wifi;
+		final WifiManager wifi;
 		wifi = (WifiManager) getSystemService(Context.WIFI_SERVICE);
 		// Get WiFi status
-		WifiInfo info = wifi.getConnectionInfo();
-		textWiFi.append(info.toString());
+		// WifiInfo info = wifi.getConnectionInfo();
+		textWiFi.setText("Wifis: ");
 
+		// Register a broadcast receiver that listens for scan results.
+		registerReceiver(new BroadcastReceiver() {
+			@Override
+			public void onReceive(Context context, Intent intent) {
+				List<ScanResult> results = wifi.getScanResults();
+				for (ScanResult result : results) {
+					textWiFi.append("\n\n " + result.toString());
+				}
+			}
+		}, new IntentFilter(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION));
+		// Initiate a scan.
+		wifi.startScan();
+
+		// if(wifi.startScan()){
 		// List available networks
-		List<WifiConfiguration> configs = wifi.getConfiguredNetworks();
-		for (WifiConfiguration config : configs) {
-			textWiFi.append("\n\n" + config.toString());
-		}
+		// List<ScanResult> configs = wifi.getScanResults();
+		// for (ScanResult config : configs) {
+		// textWiFi.append("\n\n" + config.toString());
+		// }
+		// }
 	}
 
 	// Register receiver to listen service state and signal strength
